e72e3b3d9256209ebd50e1033e100f7f6f14a176
jition1987 <34154942+jition1987@users.noreply.github.com>
2020-07-19 09:10:09 +0800

    Update Test.java

--- src/main/java/cn/cqs/pojo/Test.java
+++ src/main/java/cn/cqs/pojo/Test.java
@@ -1,6 +1,15 @@
 package cn.cqs.pojo;
 
 public class Test {
- public void static main (String args){
-   System.println.out("helloword");
+ 	public static void main(String[] args) {
+		StringBuffer a = new StringBuffer("A");
+		StringBuffer b = new StringBuffer("B");
+		add(a, b);
+		System.out.println("a="+a + ",b=" + b);
+	}
+
+	private static void add(StringBuffer x, StringBuffer y) {
+		x.append(y);
+		y = x;
+	}
 }
